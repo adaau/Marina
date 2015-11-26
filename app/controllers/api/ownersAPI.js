227ad57ba41f50020628a5da02208f2f2202dc2b
adaau <ada.au.cheng@gmail.com>
2015-11-26 10:55:29 +0800

    deleted new bookings js file

---
+++ app/controllers/api/ownersAPI.js
@@ -0,0 +1,33 @@
+var express  = require ('express');
+var router   = express.Router();
+var mongoose = require ('mongoose');
+var Boat     = require ('../../models/boat');
+var Booking  = require ('../../models/booking');
+var User     = require ('../../models/user');
+
+module.exports = function (app) {
+  app.use('/', router);
+};
+
+function authenticatedUser(req, res, next) {
+  // If the user is authenticated, then we continue the execution
+  // Otherwise the request is always redirected to the home page
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    return res.status(401).json({message: "Please Login"});
+  }
+}
+
+// GET Index of all bookings (of my boats)
+router.route('/api/my/boats/bookings')
+  .get(authenticatedUser, function (req, res, next) {
+    Boat.find({user_id: req.user, booking_id: {$ne: null}}, function(err, boats) {
+      if (err) {
+        res.status(400).send(err);
+      }
+      else {
+        res.status(200).json(boats)
+      }
+    });
+  });
